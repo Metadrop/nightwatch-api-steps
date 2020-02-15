0fb0f689bcb2538da5609bee638364cd84a01134
Omar <omar.lopesino@metadrop.net>
2020-02-15 14:14:52 +0100

    COMMAND - Step to click field.

---
+++ commands/fillField.js
@@ -0,0 +1,17 @@
+const selectors = require('../helpers/selectors');
+const { client } = require('nightwatch-api');
+
+/**
+ * Fill a field.
+ *
+ * @param {string} label
+ *   Label of the field.
+ * @param {mixed} value
+ *   Value of the field.
+ */
+module.exports.command = function(label, value) {
+    let selector = selectors.buildInputSelector(label);
+    client.assert.visible(selector);
+    client.setValue(selector, value);
+    return this;
+};
