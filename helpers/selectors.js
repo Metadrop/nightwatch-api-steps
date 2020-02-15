1a3e4461a58863485195363d7a456a06d6c06fab
Omar <omar.lopesino@metadrop.net>
2020-02-15 14:09:38 +0100

    More function to build selectors: buttons, inputs.

--- helpers/selectors.js
+++ helpers/selectors.js
@@ -24,5 +24,39 @@ function buildTableRowSelector(text) {
     return {selector: xpath, locateStrategy: 'xpath'};
 }
 
+/**
+ * Build the selector for a button or input of type submit.
+ *
+ * @param {string} locator
+ *   Button's text, or input's value.
+ */
+function buildButtonSelector(locator) {
+    const query_conditions = '@name= "' + locator +'"'
+    + ' or @id= "' + locator +'" or ';
+    let xpath = '//button[' + query_conditions + 'normalize-space(text())="' + locator + '"]'
+    + ' | //input[@type="submit" or @type="button"][' + query_conditions + '@value="'+ locator + '"]';
+    return {selector: xpath, locateStrategy: 'xpath'};
+}
+
+/**
+ * Build input selector for a specific type.
+ *
+ * It consider that the input is in the dame container
+ * than the label.
+ *
+ * @param {*} type
+ * @param {*} label
+ */
+function buildInputSelector(label, type) {
+    let type_filter = '';
+    if (typeof type === 'string') {
+        type_filter = '[@type="' + type + '"]';
+    }
+    return selector = {selector: '//div[label[contains(text(), "' + label + '")]]/*[self::input' + type_filter + ' or self::select]', locateStrategy: 'xpath'};
+}
+
+
 module.exports.buildLinkSelector = buildLinkSelector;
 module.exports.buildTableRowSelector = buildTableRowSelector;
+module.exports.buildInputSelector = buildInputSelector;
+module.exports.buildButtonSelector = buildButtonSelector;
